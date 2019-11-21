305dbf600299216a5b4937c93d9d32c459c702d8
Luke Lin <lukelin1991@yahoo.com>
2019-11-21 22:58:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ def monopoly_with_third_tier
 	{ :railroads => {
 			:names => {
 				:reading_railroad => {},
-				:name2 => {},
+				:pennsylvania_railroad => {},
 				:name3 => {},
 				:name4 => {}
 			}
