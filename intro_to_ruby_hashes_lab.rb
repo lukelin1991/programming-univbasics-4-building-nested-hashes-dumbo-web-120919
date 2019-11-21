70ddae8b55735a986e2b72314c40cc595fc1d7bf
Luke Lin <lukelin1991@yahoo.com>
2019-11-21 23:02:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_third_tier
 				:pennsylvania_railroad => {},
 				:b_and_o_railroad => {},
 				:shortline_railroad => {}
-			}
+			},
 			:pieces => 4,
 			:rent_in_dollars => {
 				one_piece_owned: 25,
